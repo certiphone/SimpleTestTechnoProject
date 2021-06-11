448d8e8b2d61a273f21d1210a8d101dc30c39e01
ruffieuxl08 <ruffieuxl08@studentfr.ch>
2021-06-11 15:05:52 +0200

    upload sms fonction
    toutes les fonctions sms sont implémenter

---
+++ ReceiveSMS/app/src/main/java/com/example/receivesms/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.receivesms;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.example.mysmsapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView myTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myTextView = findViewById(R.id.textView);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_SMS}, PackageManager.PERMISSION_GRANTED);
+    }
+
+    public void Read_SMS(View view){
+
+        Cursor cursor = getContentResolver().query(Uri.parse("content://sms"), null, null,null,null);
+        cursor.moveToFirst();
+
+        myTextView.setText(cursor.getString(12));
+
+    }
+}
